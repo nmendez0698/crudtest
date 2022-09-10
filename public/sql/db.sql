3008fdc6b22d79ce498e5c5007d2ce6e9b13730d
Neris <nerismoises1998@gmail.com>
2022-09-10 07:42:41 -0600

    DB movida

--- public/sql/db.sql
+++ public/sql/db.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `scclicks_wp_dbtest`
+-- Base de datos: `scclicks_dblaravel`
 --
 CREATE DATABASE IF NOT EXISTS `scclicks_wp_dbtest` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 USE `scclicks_wp_dbtest`;
